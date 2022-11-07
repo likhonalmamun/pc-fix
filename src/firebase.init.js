0eb6e3d6f3b3035cbd7c1f859cede82c043f91fd
likhonalmamun <alikhon055@gmail.com>
2022-11-07 21:41:18 +0600

    first commit on basic project setup

---
+++ src/firebase.init.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAyCtJRrptSX_-Y_B1GwQr7H8qV5vmT6-I",
+  authDomain: "assignment-11-client-dcb6d.firebaseapp.com",
+  projectId: "assignment-11-client-dcb6d",
+  storageBucket: "assignment-11-client-dcb6d.appspot.com",
+  messagingSenderId: "148890782777",
+  appId: "1:148890782777:web:805cf3ae4e70445250d1e8",
+};
+const app = initializeApp(firebaseConfig);
+export default app;
